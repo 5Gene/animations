184646fe19d4ce5807e5a536c946893a54fd217a
ZuYun <jonas.jzy@gmail.com>
2023-03-27 22:13:00 +0800

    ☀️

--- cartoon/src/main/java/osp/sparkj/cartoon/curves/Curve.kt
+++ cartoon/src/main/java/osp/sparkj/cartoon/curves/Curve.kt
@@ -1,6 +1,7 @@
 package osp.sparkj.cartoon.curves
 
 import android.view.animation.Interpolator
+import androidx.compose.animation.core.CubicBezierEasing
 import androidx.compose.animation.core.Easing
 import kotlin.math.absoluteValue
 
@@ -12,32 +13,51 @@ import kotlin.math.absoluteValue
  * <p><a href="https://github.com/5hmlA">github</a>
  * @see androidx.compose.animation.core.CubicBezierEasing
  */
+
+//https://cubic-bezier.com/#.25,1,.5,1
+val EaseOutQuart = CubicBezierEasing(0.25f, 1f, 0.5f, 1f)
+
+/**
+ * https://cubic-bezier.com/
+ * 这个网站可以直接看曲线
+ */
 class Curve(
     private val a: Float,
     private val b: Float,
     private val c: Float,
     private val d: Float
-) : Interpolator, Easing {
-
-    private val _cubicErrorBound = 0.001
+) : Interpolator, Easing by CubicBezierEasing(a, b, c, d) {
 
     override fun getInterpolation(t: Float): Float {
-        var start = 0.0F
-        var end = 1.0F
-        while (true) {
-            val midpoint = (start + end) / 2
-            val estimate = _evaluateCubic(a, c, midpoint)
-            if ((t - estimate).absoluteValue < _cubicErrorBound)
-                return _evaluateCubic(b, d, midpoint).toFloat()
-            if (estimate < t) start = midpoint else end = midpoint
-        }
-    }
-
-    private fun _evaluateCubic(a: Float, b: Float, m: Float): Float {
-        return 3 * a * (1 - m) * (1 - m) * m + 3 * b * (1 - m) * m * m + m * m * m
-    }
-
-    override fun transform(fraction: Float): Float {
-        return getInterpolation(fraction)
+        return transform(t)
     }
 }
+//class Curve(
+//    private val a: Float,
+//    private val b: Float,
+//    private val c: Float,
+//    private val d: Float
+//) : Interpolator, Easing {
+//
+//    private val _cubicErrorBound = 0.001
+//
+//    override fun getInterpolation(t: Float): Float {
+//        var start = 0.0F
+//        var end = 1.0F
+//        while (true) {
+//            val midpoint = (start + end) / 2
+//            val estimate = _evaluateCubic(a, c, midpoint)
+//            if ((t - estimate).absoluteValue < _cubicErrorBound)
+//                return _evaluateCubic(b, d, midpoint).toFloat()
+//            if (estimate < t) start = midpoint else end = midpoint
+//        }
+//    }
+//
+//    private fun _evaluateCubic(a: Float, b: Float, m: Float): Float {
+//        return 3 * a * (1 - m) * (1 - m) * m + 3 * b * (1 - m) * m * m + m * m * m
+//    }
+//
+//    override fun transform(fraction: Float): Float {
+//        return getInterpolation(fraction)
+//    }
+//}
---
+++ cartoon/src/main/java/osp/sparkj/cartoon/wings/Effects.kt
@@ -0,0 +1,34 @@
+package osp.sparkj.cartoon.wings
+
+import android.graphics.ColorMatrix
+import android.graphics.ColorMatrixColorFilter
+import android.graphics.RenderEffect
+import android.graphics.Shader
+import android.os.Build
+import androidx.annotation.RequiresApi
+
+/**
+ * 流体液态相连的粘接效果 任意两形状接近或者相交 表现出流体液态相连效果
+ * https://github.com/jurajkusnier/fluid-bottom-navigation
+ */
+@RequiresApi(Build.VERSION_CODES.S)
+fun fluidRenderEffect(): RenderEffect {
+    val blurEffect = RenderEffect
+        .createBlurEffect(80f, 80f, Shader.TileMode.MIRROR)
+
+    val alphaMatrix = RenderEffect.createColorFilterEffect(
+        ColorMatrixColorFilter(
+            ColorMatrix(
+                floatArrayOf(
+                    1f, 0f, 0f, 0f, 0f,
+                    0f, 1f, 0f, 0f, 0f,
+                    0f, 0f, 1f, 0f, 0f,
+                    0f, 0f, 0f, 50f, -5000f
+                )
+            )
+        )
+    )
+
+    return RenderEffect
+        .createChainEffect(alphaMatrix, blurEffect)
+}
